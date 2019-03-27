99f4b76310f95d2ebca998c93742a067a3fa8f72
Milena Iovino <iovinomilena@gmail.com>
2019-03-27 23:28:29 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace todo
+{
+    class Program
+    {
+        const string EXIT = "EXIT";
+        const string LIST = "LIST";
+        static void Main(string[] args)
+        {
+            
+            var _items = new List<string>();
+            Console.WriteLine();
+            Console.WriteLine("Simple todo list");
+            Console.WriteLine("Available commands: list, exit");
+
+            string userInput = "";
+            while (!userInput.Equals(EXIT, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Please enter an item: ");
+                userInput = Console.ReadLine();
+                if (userInput.Equals(LIST, StringComparison.OrdinalIgnoreCase)) {
+                    Console.WriteLine("The list currently contains: ");
+                    Console.WriteLine(string.Join(", ", _items));
+                } else {
+                    _items.Add(userInput);
+                }
+            }
+
+            Console.WriteLine("Bye! :)");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#### C# console application exercise
+
+Type LIST to show all the stored user items or EXIT to quit the application
